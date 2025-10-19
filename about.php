0a9a11dfdda54376148418226808a0146dc5e21c
uEdUv8ct <115344091+uEdUv8ct@users.noreply.github.com>
2025-10-20 00:27:07 +0200

    解决了 空指针异常 Bug

---
+++ about.php
@@ -0,0 +1,44 @@
+<?php include("header.html")
+?>
+<body>
+<div class="about">
+<div class="image1">
+<marquee direction="down" onMouseOver="this.stop()" onMouseOut="this.start()">
+<img src="image 1.jpg" height="200px" width="300px" />
+</marquee>
+</div>
+<div class="image2">
+<marquee direction="left">
+<img src="image 2.jpg" height="200px" width="300px" />
+</marquee>
+</div>
+<div class="image3">
+<marquee direction="right">
+<img src="image 3.jpg" height="200px" width="300px" />
+</marquee>
+</div>
+<div class="image4">
+<marquee direction="up">
+<img src="image 4.jpg" height="200px" width="300px" />
+</marquee>
+</div>
+<div class="image5">
+<marquee direction="down">
+<img src="image 5.jpg" height="200px" width="300px" />
+</marquee>
+</div>
+<div class="image6">
+<marquee direction="left">
+<img src="image 6.jpg" height="200px" width="300px" />
+</marquee>
+</div>
+<div class="image7">
+<marquee direction="right">
+<img src="image7.jpg" height="200px" width="300px" />
+</marquee>
+</div>
+</div>
+</body>
+<?php include("footer.html")
+?>
+
